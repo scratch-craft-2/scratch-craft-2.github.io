790ac4cd4907a2e6e923e4ebf58f62dd4d987afb
scratch-craft-2 <156839189+scratch-craft-2@users.noreply.github.com>
2024-12-07 23:50:27 +0300

    Create js.js

---
+++ files/js.js
@@ -0,0 +1,19 @@
+function playMusic(){
+  var music = new Audio('/files/audio/click.wav');
+  music.play();
+setTimeout(document.location.href = "/туториалы" , 2000) ;}
+function playMusic2(){
+  var music = new Audio('/files/audio/click.wav');
+  music.play();
+setTimeout(document.location.href = "/звуки" , 2000) ;
+  } 
+function playMusic3(){
+  var music = new Audio('/files/audio/click.wav');
+  music.play();
+setTimeout(document.location.href = "/игры" , 2000) ;
+  } 
+function playMusic4(){
+  var music = new Audio('/files/audio/click.wav');
+  music.play();
+setTimeout(document.location.href = "/инструменты" , 2000) ;
+  } 
