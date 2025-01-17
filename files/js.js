5d0f09c78d8e3cb380e788576b7b048a28ab961a
scratch-craft-2 <156839189+scratch-craft-2@users.noreply.github.com>
2025-01-17 11:39:03 +0300

    Update js.js

--- files/js.js
+++ files/js.js
@@ -17,4 +17,3 @@ function playMusic4(){
   music.play();
 setTimeout(document.location.href = "/инструменты" , 2000) ;
   } 
-function changeStyle(theme) { const link = document.getElementById('theme-style'); if (theme === 'dark') { link.href = '/files/css/darkmodeStyle.css'; } else if (theme === 'light') { link.href = '/files/css/mainStyle.css'; } localStorage.setItem('currentTheme', theme); } window.onload = function() { const savedTheme = localStorage.getItem('currentTheme'); if (savedTheme) { changeStyle(savedTheme); } };
