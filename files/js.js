b96cefb233e625032220e68410e71d5559a92434
scratch-craft-2 <156839189+scratch-craft-2@users.noreply.github.com>
2025-01-30 17:33:35 +0300

    Update js.js

--- files/js.js
+++ files/js.js
@@ -30,7 +30,7 @@ if (gravity == "true") {
     script_object.innerHTML = data;
   });
   console.log(script_object);
-  document.body.appendChild(script_object);
+  document.head.appendChild(script_object);
   void(0);
 }
 }
